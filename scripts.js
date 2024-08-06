366fc906218674dbacfb51711e117a76209c13ff
chintalachaitanyasai <104911764+chintalachaitanyasai@users.noreply.github.com>
2024-08-06 16:52:41 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    showSection('about'); // Show 'About Me' section by default
+});
+
+function showSection(sectionId) {
+    // Hide all sections
+    var sections = document.querySelectorAll('.section');
+    sections.forEach(function(section) {
+        section.style.display = 'none';
+    });
+
+    // Show the selected section
+    var selectedSection = document.getElementById(sectionId);
+    if (selectedSection) {
+        selectedSection.style.display = 'block';
+    }
+}
